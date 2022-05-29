ca4be645fe40fb8d457a1bd3d7fbe78ca203366d
Agnieszka <podoleckaa@gmail.com>
2022-05-29 09:58:31 +0200

    fixed missing STATIC_ROOT variable

--- goodmovies/settings.py
+++ goodmovies/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
